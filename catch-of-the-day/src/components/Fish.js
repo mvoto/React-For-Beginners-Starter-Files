50804df6d5ef7a20d29875134368c56535ef70b5
Mauricio Voto <mauriciovoto@gmail.com>
2017-11-17 14:03:00 -0800

    fix fish component html elements

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -11,9 +11,9 @@ class Fish extends React.Component {
         <h3 className="fish-name">
           {details.name}
           <span className="price">{formatPrice(details.price)}</span>
-          <p>{details.desc}</p>
-          <button>Add To Order</button>
         </h3>
+        <p>{details.desc}</p>
+        <button>Add To Order</button>
       </li>
     )
   }
